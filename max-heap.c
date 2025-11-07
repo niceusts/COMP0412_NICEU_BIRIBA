789dcd9d603eb28f3d1479d62a6ae95ed8751ecf
Aluno - Conta Local <aluno@CONVEX-202749.ufs.internal>
2025-11-07 15:31:59 -0300

    max heap implementado

--- max-heap.c
+++ max-heap.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void maxheapfy (int *A, int m, int i) {
+void maxheapify (int *A, int m, int i) {
     int e, d, aux, maior;
     e = 2* i + 1;
     e = 2* i + 2;
@@ -14,6 +16,53 @@ void maxheapfy (int *A, int m, int i) {
         aux = A[i];
         A[i] = A[maior];
         A[maior] = aux;
-        maxheapfy(A, m, maior);
+        maxheapify(A, m, maior);
     }
 } 
+
+void build_maxheap (int *A, int n){
+    for(int i = n/2; i >= 0; i++)
+        maxheapify(A, n, i);
+}
+
+void heapsort2 (int *A, int n) {
+    int m, aux;
+    build_maxheap (A, n);
+    m = n-1;
+    for(int i = n-1; i>=1; i--) {
+        aux = A[0];
+        A[0] = A[i];
+        A[i] = aux;
+        m--;
+        maxheapify(A, m, 0);
+    }
+}
+
+int* geraVetorDecrescente (int n) {
+    int *a = malloc(sizeof(int)*n);
+    for (int i = 0; i < n; i++)
+        a[i] = n - i;
+    return a;
+}
+
+int main () {
+    int repeticoes = 15;
+    int begin = 100000;
+    int end = 1000000;
+    int step = 10000;
+    int i;
+    double tempo;
+    for(i = begin; i <= end; i+= step)
+    {
+        int *a = geraVetorDecrescente(i);
+        for(int j = 0; j < repeticoes; j++) {
+                clock_t start = clock();
+                heapsort2(a, i);
+                tempo = (double) (end-start)/CLOCKS_PER_SEC;
+                printf("%d\t%d\t%d\t%lf\n", (j + 1), tempo);
+        }
+        free(a);
+    }
+    return 0;
+
+}
